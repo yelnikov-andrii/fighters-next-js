525f9aa391e4aa59c42f016a0bad870d04ee99a3
Andrii <whiteyelnikov@gmail.com>
2026-04-18 19:00:44 +0300

    changed handleChange in forgot password

--- src/components/templates/forgot-password/ForgotPassword.tsx
+++ src/components/templates/forgot-password/ForgotPassword.tsx
@@ -1,47 +1,46 @@
-'use client'
-import FormButton from '@/components/elements/form-button/FormButton';
-import FormInput from '@/components/elements/input/FormInput';
-import { useTranslations } from 'next-intl';
-import Link from 'next/link';
-import * as React from 'react';
-import { useState } from 'react';
+"use client";
+import FormButton from "@/components/elements/form-button/FormButton";
+import FormInput from "@/components/elements/input/FormInput";
+import { useTranslations } from "next-intl";
+import Link from "next/link";
+import * as React from "react";
+import { ChangeEvent, useState } from "react";
 
 function ForgotPassword() {
-    const t = useTranslations('common');
-    const [email, setEmail] = useState('');
+  const t = useTranslations("common");
+  const [email, setEmail] = useState("");
 
-    function handleChange(obj: { value: string, name: string }) {
-        setEmail(obj.value);
-    }
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setEmail(e.target.value);
+  }
 
-    return (
-        <section className='py-10'>
-            <div className='container'>
-                <h1 className='text-4xl font-bold font-osvald text-center uppercase mb-6'>
-                    {t('reset_password')}
-                </h1>
-                <p className='text-center mb-8'>
-                    {t('we_will_send')}
-                </p>
-                <form className='py-2 max-w-[400px] mx-auto flex flex-col gap-6'>
-                    <FormInput
-                        label='Email'
-                        placeholder='Email'
-                        type='email'
-                        value={email}
-                        name='email'
-                        change={handleChange}
-                    />
-                    <FormButton type='submit'>
-                        {t('submit')}
-                    </FormButton>
-                    <Link href="/login" className='self-center underline hover:text-silver transition-all'>
-                        {t('cancel')}
-                    </Link>
-                </form>
-            </div>
-        </section>
-    );
+  return (
+    <section className="py-10">
+      <div className="container">
+        <h1 className="text-4xl font-bold font-osvald text-center uppercase mb-6">
+          {t("reset_password")}
+        </h1>
+        <p className="text-center mb-8">{t("we_will_send")}</p>
+        <form className="py-2 max-w-[400px] mx-auto flex flex-col gap-6">
+          <FormInput
+            label="Email"
+            placeholder="Email"
+            type="email"
+            value={email}
+            name="email"
+            change={handleChange}
+          />
+          <FormButton type="submit">{t("submit")}</FormButton>
+          <Link
+            href="/login"
+            className="self-center underline hover:text-silver transition-all"
+          >
+            {t("cancel")}
+          </Link>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default ForgotPassword;
